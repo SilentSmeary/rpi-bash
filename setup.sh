3881da5f5b380e2dd28ba9be9a79c81ebc2e48f7
James <barowikajames@gmail.com>
2024-07-11 02:14:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,24 +5,16 @@ BLUE='\033[0;34m'
 MAGENTA='\033[0;35m'
 CYAN='\033[0;36m'
 WHITE='\033[0;37m'
-NC='\033[0m' # No Color
+NOCOLOUR='\033[0m' # No Color
 
 sudo apt install nala
-
-echo -e "${GREEN}[INSTALLED]${NC} Nala"
 
 sudo nala update
 sudo nala upgrade -y
 
-echo -e "${YELLOW}[UPGRADE]${NC} System"
-
 sudo nala remove transmission-gtk -y
 sudo nala remove remmina -y
 sudo nala remove gnome-snapshot -y
-
-echo -e "${RED}[REMOVED]${NC} Transmission"
-echo -e "${RED}[REMOVED]${NC} Remmina"
-echo -e "${RED}[REMOVED]${NC} Camera"
 
 sudo nala install thonny -y 
 sudo nala install python3-pandas -y
@@ -30,31 +22,17 @@ sudo nala install chromium-browser -y
 sudo nala install okular -y
 sudo nala install git -y
 
-
-echo -e "${GREEN}[INSTALLED]${NC} Thonny"
-echo -e "${GREEN}[INSTALLED]${NC} Pandas"
-echo -e "${GREEN}[INSTALLED]${NC} Chormium Browser"
-echo -e "${GREEN}[INSTALLED]${NC} Okular"
-echo -e "${GREEN}[INSTALLED]${NC} Git"
-
 cd #
 cd Downloads/
 wget "https://github.com/shiftkey/desktop/releases/download/release-3.4.2-linux1/GitHubDesktop-linux-arm64-3.4.2-linux1.deb"
 sudo dpkg -i GitHubDesktop-linux-arm64-3.4.2-linux1.deb
 
-echo -e "${GREEN}[INSTALLED]${NC} Github Desktop"
-
-
-wget "https://download.jetbrains.com/webide/PhpStorm-2024.1.4-aarch64.tar.gz?_gl=1*nnqzli*_gcl_au*ODYyODQyMzU5LjE3MjA2Mjk2NTk.*_ga*MTc1MDU2NzIyMy4xNzIwNjI5NjYx*_ga_9J976DJZ68*MTcyMDYyOTY2MC4xLjEuMTcyMDYyOTczNC41NS4wLjA."
+wget "https://download.jetbrains.com/webide/PhpStorm-2024.1.4-aarch64.tar.gz"
 sudo tar -xvf PhpStorm-2024.1.4-aarch64.tar.gz
-cd PhpStorm-241*
-cd bin
-bash phpstorm.sh
-# TODO: Add Phpstorm to Applications Menu
-# Move unzip to /opt
-# Clone the desktop file from this repo and move to usr/share/applications/phpstorm.desktop
-
-echo -e "${GREEN}[INSTALLED]${NC} PhpStorm"
+mv [PhpStorm-241*] [PhpStorm]
+cd /usr/share/applications/
+sudo wget "https://raw.githubusercontent.com/SilentSmeary/rpi-bash/main/phpstorm.desktop"
+sudo chmod +x /usr/share/applications/phpstorm.desktop
 
 cd #
 
@@ -62,7 +40,6 @@ sudo nala update
 sudo nala upgrade
 
 sudo nala install apache2 -y
-
 sudo systemctl enable apache2
 sudo systemctl start apache2
 
@@ -70,36 +47,47 @@ sudo nala install mysql-server -y
 
 sudo nala install php libapache2-mod-php php-mysql -y
 
-php -v
-
 sudo nala install phpmyadmin -y
-
 sudo ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
-
-echo -e "${GREEN}[INSTALLED]${NC} Apache"
-echo -e "${GREEN}[INSTALLED]${NC} MySQL Server"
-echo -e "${GREEN}[INSTALLED]${NC} php"
-echo -e "${GREEN}[INSTALLED]${NC} phpmyadmin"
 
 sudo nala install sssd-ad -y
 sudo nala install sssd-tools -y
 sudo nala install realmd -y
 sudo nala install adcli -y
 
-echo -e "${GREEN}[INSTALLED]${NC} sssd-ad"
-echo -e "${GREEN}[INSTALLED]${NC} sssd-tools"
-echo -e "${GREEN}[INSTALLED]${NC} realmd"
-echo -e "${GREEN}[INSTALLED]${NC} adcli"
-
 sudo realm -v discover 193.228.225.131
-
 PASSWORD="Password123@#!!"
-
 echo $PASSWORD | sudo realm join -U demo 193.228.225.131
 
-echo -e "${GREEN}[CYAN]${NC} Joined tlevel.digital"
-
 sudo apt autoremove -y
+
+echo -e "${YELLOW}[SYSTEM]${NOCOLOUR} Upgraded"
+
+echo -e "${GREEN}[INSTALLED]${NOCOLOUR} Nala"
+echo -e "${GREEN}[INSTALLED]${NOCOLOUR} Thonny"
+echo -e "${GREEN}[INSTALLED]${NOCOLOUR} Pandas"
+echo -e "${GREEN}[INSTALLED]${NOCOLOUR} Chormium Browser"
+echo -e "${GREEN}[INSTALLED]${NOCOLOUR} Okular"
+echo -e "${GREEN}[INSTALLED]${NOCOLOUR} Git"
+echo -e "${GREEN}[INSTALLED]${NOCOLOUR} Github Desktop"
+echo -e "${GREEN}[INSTALLED]${NOCOLOUR} PhpStorm"
+echo -e "${GREEN}[INSTALLED]${NOCOLOUR} Apache"
+echo -e "${GREEN}[INSTALLED]${NOCOLOUR} MySQL Server"
+echo -e "${GREEN}[INSTALLED]${NOCOLOUR} php"
+echo -e "${GREEN}[INSTALLED]${NOCOLOUR} phpmyadmin"
+echo -e "${GREEN}[INSTALLED]${NOCOLOUR} sssd-ad"
+echo -e "${GREEN}[INSTALLED]${NOCOLOUR} sssd-tools"
+echo -e "${GREEN}[INSTALLED]${NOCOLOUR} realmd"
+echo -e "${GREEN}[INSTALLED]${NOCOLOUR} adcli"
+
+echo -e "${RED}[REMOVED]${NOCOLOUR} Transmission"
+echo -e "${RED}[REMOVED]${NOCOLOUR} Remmina"
+echo -e "${RED}[REMOVED]${NOCOLOUR} Camera"
+echo -e "${RED}[REMOVED]${NOCOLOUR} Unused Packages"
+
+
+echo -e "${CYAN}[DOMAIN]${NOCOLOUR} Joined tlevel.digital domain"
+echo ""
 
 prompt_restart() {
     while true; do
@@ -111,7 +99,7 @@ prompt_restart() {
                 break
                 ;;
             [Nn]* ) 
-                echo "Restart canceled."
+                echo "Restart stopped."
                 exit
                 ;;
             * ) 
