277fa34df7caf02f3f7e12e0e1892da443786eed
James <barowikajames@gmail.com>
2024-07-17 22:23:04 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -42,7 +42,9 @@ sudo wget "http://192.168.1.99:8000/projectlibre_1.9.3-1.deb"
 sudo dpkg -i projectlibre_1.9.3-1.deb
 sudo apt --fix-broken install -y
 
-cd #
+sudo mv phpmyadmin/ /opt/
+sudo mv .desktop/phpmyadmin.desktop /usr/share/applications/
+sudo chmod +x /usr/share/applications/phpmyadmin.desktop
 
 sudo nala update
 sudo nala upgrade -y
@@ -51,12 +53,7 @@ sudo nala install apache2 -y
 sudo systemctl enable apache2
 sudo systemctl start apache2
 
-sudo nala install mysql-server -y
-
 sudo nala install php libapache2-mod-php php-mysql -y
-
-sudo nala install phpmyadmin -y
-sudo ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
 
 sudo nala install sssd-ad -y
 sudo nala install sssd-tools -y
